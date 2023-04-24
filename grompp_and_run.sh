af2b3d898c67b6be0b3a11027f3563a910b47692
Iason Andronis <iaan1799@sol-nix.fysik.su.se>
2023-04-24 08:28:04 +0200

    uptaded scripts

--- grompp_and_run.sh
+++ grompp_and_run.sh
@@ -30,6 +30,11 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        --pressure)
+            PRESSURE=$2
+            shift
+            shift
+            ;;
         -p|--partition)
             PARTITION=$2
             shift
@@ -57,16 +62,16 @@ echo -e "Submitting job...\n"
 
 # directory management
 DT=$(date "+%d-%m-%Y_%H:%M")
-OUT_DIR_NAME="${WORK_DIR}/water_${TEMP}K_sim_${DT}"
+OUT_DIR_NAME="${WORK_DIR}/water_${TEMP}K_${PRESSURE}bar_${DT}"
 mkdir $OUT_DIR_NAME
 
 # log file
-LOG="water_${TEMP}K_sim_job.log"
+LOG="water_${TEMP}K_${PRESSURE}bar_job.log"
 touch $OUT_DIR_NAME/$LOG
 
 # MDP files
 # Make temp copies
-TEMP_MDP_DIR="mdp_30A_${TEMP}K_temp"
+TEMP_MDP_DIR="mdp_${TEMP}K_${PRESSURE}bar_temp"
 if [ -d "${TEMP_MDP_DIR}" ]; then
 	rm -rf $TEMP_MDP_DIR
 fi
@@ -80,12 +85,16 @@ MD_MDP="${TEMP_MDP_DIR}/md.mdp"
 sed -i "s/^ref_t\s*=.*/ref_t = ${TEMP}/" "${NPT_MDP}"
 sed -i "s/^ref_t\s*=.*/ref_t = ${TEMP}/" "${MD_MDP}"
 
+# change pressure
+sed -i "s/^ref_p\s*=.*/ref_p = ${PRESSURE}/" "${NPT_MDP}"
+sed -i "s/^ref_p\s*=.*/ref_p = ${PRESSURE}/" "${MD_MDP}"
+
 # Run Jobs
 jid_em_run=$(sbatch -p $PARTITION --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable em_run.sbatch -o $OUT_DIR_NAME -f $EM_MDP -p $TOPOL -c $BOX)
 
-jid_nvt_run=$(sbatch -p $PARTITION -c 2 --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable --dependency=afterok:$jid_em_run nvt_run.sbatch -o $OUT_DIR_NAME -f $NVT_MDP -p $TOPOL)
+jid_nvt_run=$(sbatch -p $PARTITION --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable --dependency=afterok:$jid_em_run nvt_run.sbatch -o $OUT_DIR_NAME -f $NVT_MDP -p $TOPOL)
 
-jid_npt_run=$(sbatch -p $PARTITION -c 2 --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable --dependency=afterok:$jid_nvt_run npt_run.sbatch -o $OUT_DIR_NAME -f $NPT_MDP -p $TOPOL)
+jid_npt_run=$(sbatch -p $PARTITION --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable --dependency=afterok:$jid_nvt_run npt_run.sbatch -o $OUT_DIR_NAME -f $NPT_MDP -p $TOPOL)
 
-jid_md_run=$(sbatch -p $PARTITION -c 2 --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable --dependency=afterok:$jid_npt_run md_run.sbatch -o $OUT_DIR_NAME -f $MD_MDP -p $TOPOL)
+jid_md_run=$(sbatch -p $PARTITION --output=$OUT_DIR_NAME/$LOG --open-mode=append --parsable --dependency=afterok:$jid_npt_run md_run.sbatch -o $OUT_DIR_NAME -f $MD_MDP -p $TOPOL)
 
