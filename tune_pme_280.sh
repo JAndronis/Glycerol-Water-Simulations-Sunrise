68e89a1bf395d3ef5c7e1187d5cc08b476c3adfd
Jason Andronis <iasona98@gmail.com>
2023-05-20 13:53:30 +0200

    Delete tune_pme_280.sh

--- tune_pme_280.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-srun gmx_mpi tune_pme -np 96 \
-    -s mdf/water_280K_1bar_25-04-2023_15\:15/md.tpr \
-    -resetstep 2500 \
-    -mdrun "gmx_mpi mdrun -ntomp 1 -deffnm mdf/water_280K_1bar_25-04-2023_15\:15/md"
