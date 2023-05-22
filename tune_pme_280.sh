7dd2c9cbfc6f227c48526425eb68d071f5087ed0
Iason Andronis <iasona98@gmail.com>
2023-05-22 11:56:08 +0300

    merge from main

--- tune_pme_280.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gmx tune_pme -np 128 \
-    -s /cfs/home/iaan1799/data/tip4p_glycerol_3p2percent_100A/water_280K_1bar_benchmark/md.tpr \
-    -ntpr 1 \
-    -resetstep 2500 \
-    -mdrun "gmx_mpi mdrun -ntomp 1 -deffnm /cfs/home/iaan1799/data/tip4p_glycerol_3p2percent_100A/water_280K_1bar_benchmark/md"
