8e7ee417b4fa2e6c2c48fb3914e6915d988cc4ae
Iason Andronis <iasona98@gmail.com>
2023-06-22 15:32:05 +0200

    updated and cleaned

--- equilibration_plots.py
+++ equilibration_plots.py
@@ -1,40 +1,13 @@
 import matplotlib.pyplot as plt
+from typing import Union
 import pandas as pd
 import argparse
 
 
-if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-f",
-        "--energy-file",
-        type=str,
-        help="Path to Trajectory files (.xtc | .trr | ...).)",
-    )
-    parser.add_argument(
-        "-w",
-        "--window",
-        default=100,
-        type=int,
-        help="Rolling average window"
-    )
-    parser.add_argument(
-        "-k1",
-        default="density",
-        type=str,
-        help="Y-axis 1 to plot."
-    )
-    parser.add_argument(
-        "-k2",
-        type=str,
-        help="Y-axis 2 to plot."
-    )
-    args = parser.parse_args()
-    
-    data = []
-    scaling = 1e-3 # to ns
+def createDataset(path: str) -> pd.DataFrame:
+    scaling = 1e-3  # to ns
     t, energy, temp, pressure, volume, density = [], [], [], [], [], []
-    with open(args.energy_file) as f:
+    with open(path) as f:
         for line in f:
             if line.startswith("#") or line.startswith("@"):
                 continue
@@ -48,41 +21,64 @@ if __name__=="__main__":
                 density.append(float(cols[5]))
             else:
                 density.append(float(cols[4]))
-    df = pd.DataFrame({"energy": energy,
-                       "temperature": temp, 
-                       "pressure": pressure,
-                       "density": density}, 
-                      index=t)
-    
-    window = args.window
-    key1 = args.k1
-    key2 = args.k2
+    df = pd.DataFrame(
+        {
+            "energy": energy,
+            "temperature": temp,
+            "pressure": pressure,
+            "density": density,
+        },
+        index=t,
+    )
+    return df
+
+
+def plotTimeSeries(
+    df: pd.DataFrame,
+    window: int = 100,
+    key1: str = "density",
+    key2: Union[str, None] = None,
+) -> None:
     fig, ax1 = plt.subplots()
     x = df.index.to_numpy()
-    y1 = df[key1].rolling(window).mean()
-    err1 = df[key1].rolling(window).std()
-    
-    ax1.plot(y1, c='b')
-    ax1.fill_between(x, y1-err1, y1+err1, 
-                     alpha=0.3, 
-                     color='b',
-                     edgecolor=None)
-    ax1.set_ylabel(r'$\rho (kg/m^3$)', c='b', fontsize=14)
-    ax1.set_xlabel(r'$t (ns)$', fontsize=14)
+    y1 = df[key1].rolling(window).mean().to_numpy()
+    err1 = df[key1].rolling(window).std().to_numpy()
+
+    ax1.plot(y1, c="b")
+    ax1.fill_between(x, y1 - err1, y1 + err1, alpha=0.3, color="b", edgecolor=None)
+    ax1.set_ylabel(r"$\rho (kg/m^3$)", c="b", fontsize=14)
+    ax1.set_xlabel(r"$t (ns)$", fontsize=14)
     ax1.set_title(f'T = {df["temperature"].mean():.00f}', fontsize=14)
     ax1.tick_params(labelsize=14)
-    
+
     if key2 is not None:
-        ax1.tick_params(c='b')
-        y2 = df[key2].rolling(window).mean()
-        err2 = df[key2].rolling(window).std()
+        ax1.tick_params(c="b")
+        y2 = df[key2].rolling(window).mean().to_numpy()
+        err2 = df[key2].rolling(window).std().to_numpy()
         ax2 = ax1.twinx()
-        ax2.plot(y2, c='red')
-        ax2.fill_between(x, y2-err2, y2+err2, 
-                         alpha=0.3, 
-                         color='red', 
-                         edgecolor=None)
-        ax2.set_ylabel(r'$U (kJ/mol)$', c='r', fontsize=14)
-        ax2.tick_params(labelsize=14, c='r')
+        ax2.plot(y2, c="red")
+        ax2.fill_between(
+            x, y2 - err2, y2 + err2, alpha=0.3, color="red", edgecolor=None
+        )
+        ax2.set_ylabel(r"$U (kJ/mol)$", c="r", fontsize=14)
+        ax2.tick_params(labelsize=14, c="r")
     plt.savefig("energy_plots.png", dpi=150)
     plt.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--energy-file",
+        type=str,
+        help="Path to Trajectory files (.xtc | .trr | ...).)",
+    )
+    parser.add_argument(
+        "-w", "--window", default=100, type=int, help="Rolling average window"
+    )
+    parser.add_argument("-k1", default="density", type=str, help="Y-axis 1 to plot.")
+    parser.add_argument("-k2", type=str, help="Y-axis 2 to plot.")
+    args = parser.parse_args()
+    df = createDataset(args.energy_file)
+    plotTimeSeries(df, args.window, args.k1, args.k2)
